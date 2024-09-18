4b02676c1e08203fa0376ef1e2e00ff8afca8df4
tirantvcia <tirantvcia@gmail.com>
2024-09-18 08:01:53 +0200

    011 refactor move TodoApiRepository

--- src/tests/integration/todoApiRepository.test.ts
+++ src/tests/integration/todoApiRepository.test.ts
@@ -1,35 +1,5 @@
-import { createTodo, Todo, updateTodo } from "../../domain/todo";
-
-class TodoApiRepository {
-    constructor(private baseUrl: string) {}
-
-    getAll(): Promise<Todo[]> {
-        return fetch(this.baseUrl)
-        .then(response => response.json())
-    }
-
-    add(todo: Todo): Promise<Todo> {
-        return fetch(this.baseUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(todo),
-        }).then(response => response.json())
-    }
-    update( todo: Todo ): Promise<Todo>  {
-        return fetch(this.baseUrl+todo.id, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ text: todo.text, completed: todo.completed , id: todo.id}),
-        })
-            .then(response => response.json());
-    }
-
-    delete(todo: Todo): Promise<{}> {
-        return fetch(this.baseUrl + todo.id, { method: 'DELETE' })
-    }
-
-
-}
+import { createTodo, updateTodo } from "../../domain/todo";
+import { TodoApiRepository } from "../../frontend/infrastructure/TodoApiRepository";
 
 describe('The Todo Api Repository', () => {
     const baseUrl = 'http://localhost:3000/api/todos/';
